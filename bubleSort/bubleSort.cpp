674077a950c73f55f30f0462854aa296674bb68e
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2023-03-17 13:17:20 +0700

    fix bubble sort procedur

--- bubleSort/bubleSort.cpp
+++ bubleSort/bubleSort.cpp
@@ -1,7 +1,6 @@
 #include <iostream>     
 using namespace std;
 
-
 int a[20];           // Deklarasi array a dengan ukuran 20
 int n;               // Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
@@ -21,9 +20,31 @@ void input() {        // procedur untuk input
 	cout << "===================" << endl;   // Output ke layar
 
 	for (int i = 0; i < n; i++) {   // Looping dengan i dimulai dari 0 hingga n-1
-		cout << "<" << (i + 1) << ">";   // Output ke layar
+		cout << "Data ke-" << (i + 1) << ": ";   // Output ke layar
 		cin >> a[i];                    // Input dari pengguna
 	}
+}
+void bubbleSortArray() {    // procedur untuk mengurutkan array dengan metode bubble sort
+	for (int i = 1; i < n; i++) {        // Looping dengan i dimulai dari 1 hingga n-1
+		for (int j = 0; j < n - i; j++) {  // Looping dengan j dimulai dari 0 hingga n-i
+			if (a[j] > a[j + 1]) {         // Jika nilai pada a[j] lebih besar dari a[j+1]
+				int temp = a[j];           // Simpan nilai a[j] ke variabel sementara temp
+				a[j] = a[j + 1];           // Assign nilai a[j+1] ke a[j]
+				a[j + 1] = temp;           // Assign nilai temp ke a[j+1]
+			}
+		}
+	}
+}
+
+void unsorted() {
+	cout << endl;                             // Output baris kosong
+	cout << "==================================" << endl;   // Output ke layar
+	cout << "Element Array yang belum tersusun" << endl;      // Output ke layar
+	cout << "==================================" << endl;   // Output ke layar
+	for (int j = 0; j < n; j++) {            // Looping dengan j dimulai dari 0 hingga n-1
+		cout << a[j] << endl;                // Output ke layar
+	}
+	cout << endl;
 }
 
 void display() {   // procedur untuk menampilkan hasil
@@ -37,24 +58,11 @@ void display() {   // procedur untuk menampilkan hasil
 	cout << endl;                            // Output baris kosong
 }
 
-void bubbleSortArray() {    // procedur untuk mengurutkan array dengan metode bubble sort
-	for (int i = 1; i < n; i++) {        // Looping dengan i dimulai dari 1 hingga n-1
-		for (int j = 0; j < n - i; j++) {  // Looping dengan j dimulai dari 0 hingga n-i-1
-			if (a[j] > a[j + 1]) {         // Jika nilai pada a[j] lebih besar dari a[j+1]
-				int temp = a[j];           // Simpan nilai a[j] ke variabel sementara temp
-				a[j] = a[j + 1];           // Assign nilai a[j+1] ke a[j]
-				a[j + 1] = temp;           // Assign nilai temp ke a[j+1]
-			}
-		}
-	}
-}
-
-
 int main() {
+	input();      // Memanggil  procedur read() 
+	unsorted();		//Memanggil procedur unsorted() dari
+	bubbleSortArray();  // Memanggil  procedur bubbleSortArray() 
+	display();   // Memanggil  procedur display()
 
-	input();      // Memanggil  procedur read() dari class BubbleSort
-	bubbleSortArray();  // Memanggil  procedur bubbleSortArray() dari class BubbleSort
-	display();   // Memanggil  procedur display() dari class BubbleSort
-	system("pause");
 	return 0;
 }
